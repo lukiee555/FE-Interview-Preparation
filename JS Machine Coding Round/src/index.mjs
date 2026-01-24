9bcee310fe3b1c8f118b23ff78b8b3525261802d
Lokesh Soni <sonilokesh24@gmail.com>
2026-01-24 16:29:52 +0530

    Array Polyfill - Array.prototype.at

--- JS Machine Coding Round/src/index.mjs
+++ JS Machine Coding Round/src/index.mjs
@@ -3,3 +3,9 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Javascript!</h1>
 `;
+
+Array.prototype.myAt = function (index) {
+  const thisArray = this;
+  const idx = index >= 0 ? index : thisArray.length + index;
+  return thisArray[idx];
+};
