090e82b2f01e30aff8f5572b75c5d73dc5b2931e
Lokesh Soni <sonilokesh24@gmail.com>
2026-02-02 17:31:40 +0530

    List Formatter

--- JS Machine Coding Round/src/index.mjs
+++ JS Machine Coding Round/src/index.mjs
@@ -548,3 +548,41 @@ export function deepClone(value) {
   }
   return clonedObj;
 }
+
+const SEPARATOR = ", ";
+const OTHERS_SEPARATOR = " and ";
+const OTHERS_LABEL = "other";
+export function listFormat(items, options) {
+  // Filter falsey values.
+  let items = itemsParam.filter((item) => !!item);
+
+  if (!items || items.length === 0) {
+    return "";
+  }
+
+  // No processing is needed if there's only one item.
+  if (items.length === 1) {
+    return items[0];
+  }
+
+  // Sort values.
+  if (options.sorted) {
+    items.sort();
+  }
+
+  // Remove duplicate values.
+  if (options.unique) {
+    items = Array.from(new Set(items));
+  }
+  if (options.length && options.length > 0 && options.length < items.length) {
+    const firstSection = items.slice(0, options.length).join(SEPARATOR);
+    const count = items.length - options.length;
+    const secondSection = `${count} ${OTHERS_LABEL + (count > 1 ? "s" : "")}`;
+    return [firstSection, secondSection].join(OTHERS_SEPARATOR);
+  }
+
+  // Case where length is not specified.
+  const firstSection = items.slice(0, items.length - 1).join(SEPARATOR);
+  const secondSection = items[items.length - 1];
+  return [firstSection, secondSection].join(OTHERS_SEPARATOR);
+}
