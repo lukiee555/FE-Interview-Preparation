ef8bb4abd5501059114a5b7287025eac0051632a
Lokesh Soni <sonilokesh24@gmail.com>
2026-02-02 20:43:05 +0530

    Deep Equal

--- JS Machine Coding Round/src/index.mjs
+++ JS Machine Coding Round/src/index.mjs
@@ -612,3 +612,35 @@ export function mapAsync(iterable, callbackFn) {
       });
   });
 }
+
+export function deepEqual(valueA, valueB) {
+  if (Object.is(valueA, valueB)) {
+    return true;
+  }
+
+  const bothObjects =
+    Object.prototype.toString.call(valueA) === "[object Object]" &&
+    Object.prototype.toString.call(valueB) === "[object Object]";
+  const bothArrays = Array.isArray(valueA) && Array.isArray(valueB);
+
+  // At this point, they can still be primitives but of different types.
+  // If they had the same value, they would have been handled earlier in Object.is().
+  // So if they're not both objects or both arrays, they're definitely not equal.
+  if (!bothObjects && !bothArrays) {
+    return false;
+  }
+
+  // Compare the keys of arrays and objects.
+  if (Object.keys(valueA).length !== Object.keys(valueB).length) {
+    return false;
+  }
+
+  for (const key in valueA) {
+    if (!deepEqual(valueA[key], valueB[key])) {
+      return false;
+    }
+  }
+
+  // All checks passed, the arrays/objects are equal.
+  return true;
+}
