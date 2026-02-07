4a28b8a0f6d6d26a4a77800f417d4bc0398bb379
Lokesh Soni <sonilokesh24@gmail.com>
2026-02-07 23:56:30 +0530

    Modal Dialog III

--- React Machine Coding Round/Modal Dialog/src/Modal.js
+++ React Machine Coding Round/Modal Dialog/src/Modal.js
@@ -1,15 +1,28 @@
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 
 export default Modal = ({ title, open, children, onClose }) => {
+  useEffect(() => {
+    if (!open) return;
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [open, onClose]);
   if (!open) return;
   return createPortal(
-    <div className="modal-overlay">
-      <div className="modal">
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal" onClick={(e) => e.preventDefault()}>
         <h1 className="modal-title">{title}</h1>
         <div>{children}</div>
         <button onClick={onClose}>Close</button>
       </div>
     </div>,
-    document.body
+    document.body,
   );
 };
